a872ea1aea484008b2df135e6d75ce8e57556737
Iván Portillo <ivan.portillo@unir.net>
2022-12-05 12:58:41 +0100

    Hotfix check endpoint

--- src/Http/HealthCheckController.php
+++ src/Http/HealthCheckController.php
@@ -1,22 +1,22 @@
-<?php
+<?php 
 
-declare (strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Http;
 
-use OpenApi\Annotations as OA;
-use Psr\Cache\CacheItemInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 
 class HealthCheckController
 {
 
-    /**
-     *
+  	 /**
+     * 
      * Health status App.
-     *
+     * 
      * @OA\Response(
      *     response=200,
      *     description="verify that the application is up",
@@ -26,19 +26,13 @@ class HealthCheckController
      *     )
      * )
      */
-    #[Route('/api/v1/check', name:'check', methods:['GET'])]
-    function check(CacheInterface $cache): JsonResponse
-    {
-        $response = $cache->get('check', function (CacheItemInterface $cacheItemInterface) {
-          
-          $cacheItemInterface->expiresAfter(5);
-          
-          return [
-                    'message' => 'Brewer up and running',
-                ];
-
-        });
-
-        return new JsonResponse($response);
-    }
+    #[Route('/api/v1/check', name: 'check', methods: ['GET'])]
+  public function check() :JsonResponse
+  {
+      return new JsonResponse(
+        [
+          'message' => 'Brewer up and running',
+        ]
+      );
+  }
 }
